e4e3843151e0f37df4ffc16fb7187377c70cfb92
jameshmread <jamesread117@yahoo.co.uk>
2017-10-10 18:00:29 +0100

    output store now holds profx config info for data transfer

--- src/output/OutputStore.ts
+++ src/output/OutputStore.ts
@@ -4,6 +4,11 @@ import { ITestResult } from "../../interfaces/ITestResult";
 
 export class OutputStore {
 
+    public readonly SRC_FILE_PATH: string;
+    public readonly SRC_FILE: string;
+    public readonly RUNNER: string;
+    public readonly RUNNER_CONFIG: Object;
+
     public testFilePath: string;
     public lineNumber: number;
     public origionalCode: string;
@@ -11,10 +16,22 @@ export class OutputStore {
     public numberOfFailedTests: number;
     public numberOfPassedTests: number;
 
-    public passedTestsDescription: Array<String>;
-    public failedTestsDescription: Array<String>;
+    // public passedTestsDescription: Array<String>;
+    // public failedTestsDescription: Array<String>;
     public mutationScore;
     public runTime: Object;
+
+    public constructor (
+        srcPath: string,
+        srcFile: string,
+        testRunner: string,
+        runnerConfig: Object
+    ) {
+        this.SRC_FILE_PATH = srcPath;
+        this.SRC_FILE = srcFile;
+        this.RUNNER = testRunner;
+        this.RUNNER_CONFIG = runnerConfig;
+    }
 
     public setTestFile (filename: string) {
         this.testFilePath = filename;
