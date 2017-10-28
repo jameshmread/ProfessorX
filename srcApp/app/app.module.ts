e33d9daef18b88b5d0bf54518e9acdb3b5eb6fcf
jameshmread <jamesread117@yahoo.co.uk>
2017-10-28 13:37:09 +0100

    dashboard folder now contains all dashboard components, same with individual mutation

--- srcApp/app/app.module.ts
+++ srcApp/app/app.module.ts
@@ -6,9 +6,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
-import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
-import { MutationStatsSummaryComponent } from "./mutation-stats-summary/mutation-stats-summary.component";
-import { MutatedFilesSummaryComponent } from "./mutated-files-summary/mutated-files-summary.component";
+import { ProgressBarComponent } from "./dashboard/progress-bar/progress-bar.component";
+import { MutationStatsSummaryComponent } from "./dashboard/mutation-stats-summary/mutation-stats-summary.component";
+import { MutatedFilesSummaryComponent } from "./dashboard/mutated-files-summary/mutated-files-summary.component";
+import { MutationFiltersComponent } from "./individualMutation/mutation-filters/mutation-filters.component";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MutatedFilesSummaryComponent } from "./mutated-files-summary/mutated-fi
     NavbarComponent,
     ProgressBarComponent,
     MutationStatsSummaryComponent,
-    MutatedFilesSummaryComponent
+    MutatedFilesSummaryComponent,
+    MutationFiltersComponent
   ],
   imports: [
     BrowserModule,
---
+++ srcApp/app/individualMutation/mutation-filters/mutation-filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mutation-filters',
+  templateUrl: './mutation-filters.component.html',
+  styleUrls: ['./mutation-filters.component.css']
+})
+export class MutationFiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
