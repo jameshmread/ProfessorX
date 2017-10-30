96c431945b93f7037fe341f63753f5aaea3485de
jameshmread <jamesread117@yahoo.co.uk>
2017-10-30 11:38:14 +0000

    now has mutatable tokens and mutator descriptions

--- src/mutationFactory/MutationFactory.ts
+++ src/mutationFactory/MutationFactory.ts
@@ -1,14 +1,32 @@
 import {SyntaxKind} from "typescript";
-import {IsyntaxMutationMap} from "../../interfaces/IsyntaxMutationMap";
+import { IsyntaxMutationMap } from "../../interfaces/IsyntaxMutationMap";
 
 export class MutationFactory {
-    private static syntaxMutationMap: IsyntaxMutationMap = {
-        37: ["-"],
-        38: ["+"],
-        39: ["/"],
-        101: ["false"],
-        86: ["true"]
+
+    public static mutatableTokens: Array<SyntaxKind> = [
+        SyntaxKind.PlusToken,
+        SyntaxKind.MinusToken,
+        SyntaxKind.TrueKeyword,
+        SyntaxKind.FalseKeyword,
+        SyntaxKind.PlusPlusToken,
+        SyntaxKind.MinusMinusToken
+    ];
+
+    public static syntaxMutationMap: IsyntaxMutationMap = {
+        37: ["-", "/", "*"], // plus
+        38: ["+"], // minus
+        39: ["/"], // multiply
+        101: ["false"], // true
+        86: ["true"] // false
     };
+
+    private static mutatorDescriptions: Array<string> = [
+        "Binary Substitution: Swapping an operator in a binary expression",
+        "Boolean Substitution: Swapping one boolean value for another",
+        "Logical Substitution: Changing a Logical operator for another",
+        "Unary Substitution: Swapping an operator in a unary expression",
+        "Condition Removal: Removing / Replacing a condition within a control structure"
+    ];
 
     public static getSingleMutation (syntaxKind: SyntaxKind): string {
         return this.syntaxMutationMap[syntaxKind][0];
