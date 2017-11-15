40d006e106bce215f6bdc4520f4552141983fcde
jameshmread <jamesread117@yahoo.co.uk>
2017-11-15 21:03:30 +0000

    cleaner now deletes single mutated source file instead of deleting multiple

--- src/cleanup/Cleaner.spec.ts
+++ src/cleanup/Cleaner.spec.ts
@@ -1,20 +1,13 @@
 import { expect } from "chai";
 import { Cleaner } from "./Cleaner";
 import { FileHandler } from "../FileHandler/FileHandler";
-import * as fs from "fs";
-
 
 describe("Cleaner", () => {
+
     let cleaner: Cleaner;
     const suffix = FileHandler.M_TEST_FILE_SUFFIX;
     const testFileDir = "./testProject/src/";
     beforeEach(() => {
-    });
-
-    it("finds no files when there are none with the M test file suffix", () => {
-        cleaner = new Cleaner("./testProject/");
-        cleaner.findMutatedFiles();
-        expect(cleaner.filesToDelete.length).to.equal(0);
     });
 
     it("a filename that ends with the target extension is flagged as file to delete", () => {
@@ -28,5 +21,4 @@ describe("Cleaner", () => {
         expect(cleaner.isTestFile("itis.mx.ts")).to.equal(false);
         expect(cleaner.isTestFile("itis.spec.mx.ts")).to.equal(false);
     });
-
 });
--- src/cleanup/Cleaner.ts
+++ src/cleanup/Cleaner.ts
@@ -1,38 +1,24 @@
-import * as fs from "fs";
+import * as fs from "memfs";
 
 import { FileHandler } from "../FileHandler/FileHandler";
 
 export class Cleaner {
     public readonly fileExtensionToRemove = FileHandler.M_SOURCE_FILE_SUFFIX;
     public readonly FILE_PATH;
-    public filesToDelete: Array<string> = [];
 
     constructor (filePath: string) {
         this.FILE_PATH = filePath;
     }
 
-    public findMutatedFiles () {
-        const filesFoundInDirectory = fs.readdirSync(this.FILE_PATH);
-
-        for (let i = 0; i < filesFoundInDirectory.length; i++){
-            if (this.isTestFile(filesFoundInDirectory[i])){
-                this.filesToDelete.push(filesFoundInDirectory[i]);
-            }
-        }
-        return this.filesToDelete;
+    public deleteMutatedSourceFile (filePath: string) {
+        fs.unlink(filePath);
     }
 
-    public deleteMutatedFiles (filesToDelete: Array<string>) {
-        for (let i = 0; i < filesToDelete.length; i++) {
-            fs.unlinkSync(this.FILE_PATH + filesToDelete[i]);
-        }
-    }
-
-    public deleteTestFile (filePath: string) {
+    public deleteMutatedTestFile (filePath: string) {
         if (!this.isTestFile(filePath)) {
             throw new Error(`${filePath} is not a mutated test file. Aborting...`);
         }
-        fs.unlinkSync(filePath);
+        fs.unlink(filePath);
     }
 
     public isTestFile (filePath: string): boolean {
