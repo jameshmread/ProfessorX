3e64f16a7a3f1c81245d0d8fff24c390d150fa24
jameshmread <jamesread117@yahoo.co.uk>
2017-11-18 23:42:36 +0000

    refactored cleaner and made static

--- src/cleanup/Cleaner.ts
+++ src/cleanup/Cleaner.ts
@@ -3,27 +3,23 @@ import * as fs from "fs";
 import { FileHandler } from "../FileHandler/FileHandler";
 
 export class Cleaner {
-    public readonly fileExtensionToRemove = FileHandler.M_SOURCE_FILE_SUFFIX;
-    public readonly FILE_PATH;
-    public filesToDelete: Array<string> = [];
+    public static readonly fileExtensionToRemove = FileHandler.M_SOURCE_FILE_SUFFIX;
+    public static filesToDelete: Array<string> = [];
 
-    constructor (filePath: string) {
-        this.FILE_PATH = filePath;
+    public static deleteSourceFile (fileToDelete: string) {
+        fs.unlink(fileToDelete);
     }
 
-    public deleteSourceFile (fileToDelete: string) {
-            fs.unlink(fileToDelete);
-    }
-
-    public deleteTestFile (filePath: string) {
-        if (!this.isTestFile(filePath)) {
+    public static deleteTestFile (filePath: string) {
+        if (!Cleaner.isTestFile(filePath)) {
             throw new Error(`${filePath} is not a mutated test file. Aborting...`);
         }
         fs.unlink(filePath);
     }
 
-    public isTestFile (filePath: string): boolean {
-        return filePath.substring(filePath.length - this.fileExtensionToRemove.length) === this.fileExtensionToRemove;
+    public static isTestFile (filePath: string): boolean {
+        return filePath.substring(filePath.length - Cleaner.fileExtensionToRemove.length)
+        === Cleaner.fileExtensionToRemove;
     }
 
 }
