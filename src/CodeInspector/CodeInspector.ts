c2919cfc94f7b56e3efd0dd09400b6153041abc1
jameshmread <jamesread117@yahoo.co.uk>
2017-11-18 23:22:46 +0000

    code inspector is slightly more static

--- src/CodeInspector/CodeInspector.ts
+++ src/CodeInspector/CodeInspector.ts
@@ -1,23 +1,23 @@
 import { Node, SourceFile, SyntaxKind } from "typescript";
 
 export class CodeInspector {
-    private retrievedObjects: Array<{pos: number, end: number}> = [];
+    private static retrievedObjects: Array<{pos: number, end: number}> = [];
     constructor (private sourceObject: SourceFile) {}
 
     public findObjectsOfSyntaxKind (kind: SyntaxKind) {
-        this.retrievedObjects = [];
-        this.findTokenObjectsOfKind(this.sourceObject, kind);
-        return this.retrievedObjects;
+        CodeInspector.retrievedObjects = [];
+        CodeInspector.findTokenObjectsOfKind(this.sourceObject, kind);
+        return CodeInspector.retrievedObjects;
     }
 
-    private findTokenObjectsOfKind (object: Node, kind: SyntaxKind)
+    private static findTokenObjectsOfKind (object: Node, kind: SyntaxKind)
     : Array<{pos: number, end: number}> {
         if (object.kind === kind) {
-            this.retrievedObjects.push({pos: object.pos, end: object.end});
+            CodeInspector.retrievedObjects.push({pos: object.pos, end: object.end});
         }
         object.getChildren().forEach((element) => {
             this.findTokenObjectsOfKind(element, kind);
         });
-        return this.retrievedObjects;
+        return CodeInspector.retrievedObjects;
     }
 }
