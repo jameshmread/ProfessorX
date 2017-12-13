b4b5b4520bf414a474a7b45a7098cecb80386a84
jameshmread <jamesread117@yahoo.co.uk>
2017-12-13 02:52:18 +0000

    can now split nodes evenly between the number of logical cores in system

--- src/index.ts
+++ src/index.ts
@@ -11,43 +11,76 @@ import * as worker from "child_process";
 import * as os from "os";
 import { SourceObject } from "../DTOs/SourceObject";
 import { FileObject } from "../DTOs/FileObject";
+import { MutationFactory } from "./mutationFactory/MutationFactory";
 
 export class ProfessorX {
+    public logicalCores: number = os.cpus().length;
+    public workers = [];
     public startTimestamp: number;
     public fileHandler: FileHandler;
     public sourceObj: SourceObject;
+    public fileObj: FileObject;
     public codeInspector: CodeInspector;
-    public nodes: Array<IMutatableNode>;
+    public nodes = new Array<IMutatableNode>();
     public multiNodeHandler: MultipleNodeHandler;
 
     public constructor () {
         const configManager = new ConfigManager();
         this.startTimestamp = new Date().getTime();
-        this.fileHandler = new FileHandler(new FileObject(ConfigManager.filePath, ConfigManager.fileToMutate));
+        this.fileObj = new FileObject(ConfigManager.filePath, ConfigManager.fileToMutate);
+        this.fileHandler = new FileHandler(this.fileObj);
         this.sourceObj = new SourceObject(this.fileHandler.getSourceObject());
-        // above two will need to be given a new source object for every file
+        this.codeInspector = new CodeInspector(this.sourceObj.origionalSourceObject);
+        // above two will need to be given a new source object / file path for every file
 
     }
 
-    public async main () {
+    public  main () {
         // will be mutateFiles -> mutateNodesInsideFiles
         // JSON.stringify(new OutputStore("path", "file", "runner", {}))
-        const workers = [];
-        for (let i = 0; i < 4; i++){
-            workers.push(worker.fork("./src/Worker.ts"));
-            workers[i].addListener("message", () => {});
-        }
-        for (let i = 0; i < 4; i++) {
-            workers[i].send("55");
-        }
-
+        // for (let i = 0; i < this.logicalCores; i++){
+        //     this.workers.push(worker.fork("./src/Worker.ts"));
+        //     this.workers[i].addListener("message", () => {});
+        // }
+       
         // this.multiNodeHandler = new MultipleNodeHandler(
         //     this.sourceObj,
-        //     this.fileHandler
+        //     this.fileObj
         // );
-        this.nodes = this.multiNodeHandler.getAllNodes();
-        await this.mutateAllNodeTypes();
-        this.finishRun();
+        this.nodes = this.getAllNodes();
+        this.splitNodes();
+        // await this.mutateAllNodeTypes();
+        // this.finishRun();
+    }
+
+    public getAllNodes () {
+        MutationFactory.mutatableTokens.forEach((syntaxItem) => {
+            this.codeInspector.findObjectsOfSyntaxKind(syntaxItem).forEach((token) => {
+                this.nodes.push({
+                    syntaxType : syntaxItem,
+                    positions : token
+                });
+            });
+        });
+        return this.nodes;
+  }
+
+    private splitNodes () {
+        const splitNodes = [];
+        let coreChosen = 0;
+        const tempNodes = this.nodes;
+        for (let i = 0; i < this.logicalCores; i++) {
+            splitNodes.push([]);
+        }
+        for (let i = 0; i < tempNodes.length; i++) {
+            if (coreChosen >= splitNodes.length) {
+                coreChosen = 0;
+            }
+            splitNodes[coreChosen].push(tempNodes[i]);
+            coreChosen++;
+        }
+        console.log(splitNodes);
+        return splitNodes;
     }
 
     private async mutateAllNodeTypes () {
