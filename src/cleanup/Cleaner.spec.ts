f1e51b943b60e29b9ee0dc7ed6ab5fe0db2f12dd
jameshmread <jamesread117@yahoo.co.uk>
2017-12-21 18:56:04 +0000

    added functions to remove any found mutated files in a dir

--- src/cleanup/Cleaner.spec.ts
+++ src/cleanup/Cleaner.spec.ts
@@ -2,13 +2,29 @@ import { expect } from "chai";
 import { Cleaner } from "./Cleaner";
 
 describe("Cleaner", () => {
-    it("a filename that ends with the target extension is flagged as file to delete", () => {
-        expect(Cleaner.isTestFile("itis.m.ts")).to.equal(true);
-        expect(Cleaner.isTestFile("itis.spec.m.ts")).to.equal(true);
+
+    it("should return true when given a source.m.ts file", () => {
+        expect(Cleaner.isMutatedFile("source.m.ts")).to.equal(true);
     });
 
-    it("a filename that doesn't end with the target extension is not flagged as file to delete", () => {
-        expect(Cleaner.isTestFile("itis.mx.ts")).to.equal(false);
-        expect(Cleaner.isTestFile("itis.spec.mx.ts")).to.equal(false);
+    it("should return true when given a test.spec.m.ts file", () => {
+        expect(Cleaner.isMutatedFile("source.spec.m.ts")).to.equal(true);
+    });
+    it("should return false when given a source.ts filename", () => {
+        expect(Cleaner.isMutatedFile("test.ts")).to.equal(false);
+    });
+
+    it("should return false when given a test.spec.ts filename", () => {
+        expect(Cleaner.isMutatedFile("test.spec.ts")).to.equal(false);
+    });
+
+    it("should return 0 files when looking in a directory .ts files", () => {
+        const fileArray = ["./hello.ts", "./hello.spec.ts"];
+        expect(Cleaner.filterMutatedFiles(fileArray).length).to.equal(0);
+    });
+
+    it("should return 2 files when looking in a directory with 2 m.ts files", () => {
+        const fileArray = ["./hello.ts", "./hello.spec.ts", "./hello.m.ts", "./hello.spec.m.ts"];
+        expect(Cleaner.filterMutatedFiles(fileArray).length).to.equal(2);
     });
 });
--- src/cleanup/Cleaner.ts
+++ src/cleanup/Cleaner.ts
@@ -1,9 +1,9 @@
 import * as fs from "fs";
 
 import { FileObject } from "../../DTOs/FileObject";
+import { ConfigManager } from "../configManager/ConfigManager";
 
 export class Cleaner {
-    public static readonly fileExtensionToRemove = FileObject.M_SOURCE_FILE_SUFFIX;
     public static filesToDelete: Array<string> = [];
 
     public static deleteSourceFile (fileToDelete: string) {
@@ -11,15 +11,39 @@ export class Cleaner {
     }
 
     public static deleteTestFile (filePath: string) {
-        if (!Cleaner.isTestFile(filePath)) {
+        if (!Cleaner.isMutatedFile(filePath)) {
             throw new Error(`${filePath} is not a mutated test file. Aborting...`);
         }
         fs.unlink(filePath);
     }
 
-    public static isTestFile (filePath: string): boolean {
-        return filePath.substring(filePath.length - Cleaner.fileExtensionToRemove.length)
-        === Cleaner.fileExtensionToRemove;
+    public static cleanRemainingFiles () {
+        const fileArray = fs.readdir(ConfigManager.filePath, (err, directory) => {
+            console.log("Error Removing Files after termination: ", err);
+        });
+        Cleaner.removeFoundFiles(
+            Cleaner.filterMutatedFiles(fileArray)
+        );
+    }
+
+    public static filterMutatedFiles (fileArray) {
+        return fileArray.filter((element) => {
+            return Cleaner.isMutatedFile(element);
+        });
+    }
+
+
+    public static isMutatedFile (filePath: string): boolean {
+        return filePath.substring(filePath.length - FileObject.M_SOURCE_FILE_SUFFIX.length)
+        === FileObject.M_SOURCE_FILE_SUFFIX;
+    }
+
+    private static removeFoundFiles (fileArray) {
+        fileArray.forEach((file) => {
+            fs.unlink(file, (err) => {
+                console.log("ERROR removing files: ", err);
+            });
+        });
     }
 
 }
