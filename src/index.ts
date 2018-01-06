d00b94b36ca8a71bbd7da68197c5ecf57b4ff36b
jameshmread <jamesread117@yahoo.co.uk>
2018-01-06 19:47:27 +0000

    moved node functionality out, removed unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,43 @@
-import { IMutatableNode } from "../interfaces/IMutatableNode";
 import { ConfigManager } from "./configManager/ConfigManager";
 import { FileHandler } from "./FileHandler/FileHandler";
 import { CodeInspector } from "./CodeInspector/CodeInspector";
-import { SourceCodeHandler } from "./SourceCodeHandler/SourceCodeHandler";
-import { OutputStoreManager } from "./output/OutputStoreManager";
-import { MutationFactory } from "./mutationFactory/MutationFactory";
 import { Supervisor } from "./Supervisor";
-
-import { SourceObject } from "../DTOs/SourceObject";
-import { OutputStore } from "../DTOs/OutputStore";
-import { FileObject } from "../DTOs/FileObject";
-import { IFileDescriptor } from "../interfaces/IFileDescriptor";
+import { NodeHandler } from "./nodeHandler/NodeHandler";
+import { SourceObject } from "./../DTOs/SourceObject";
+import { FileObject } from "./../DTOs/FileObject";
+import { IFileDescriptor } from "./../interfaces/IFileDescriptor";
 
 export class ProfessorX {
 
-    public fileNameNodes = new Array<IMutatableNode>();
-    public fileDescriptors: Array<IFileDescriptor>;
     public supervisor: Supervisor;
+    public nodeHandler: NodeHandler;
 
     public constructor () {
         console.log("Setting up environment. \n");
         const configManager = new ConfigManager();
         ConfigManager.getFilesToMutate();
-        this.fileDescriptors = new Array<IFileDescriptor>();
     }
 
     public async main () {
-        // will be mutateFiles -> mutateNodesInsideFiles
         console.log("Creating File Objects");
-        this.createAllFileDescriptors();
+        this.nodeHandler = new NodeHandler(this.createAllFileDescriptors());
         console.log("Finding Nodes... \n");
-        this.traverseFiles();
-        console.log("Found ", this.fileNameNodes.length, " mutatable nodes. ");
+        this.nodeHandler.traverseFilesForNodes();
+        console.log("Found ", this.nodeHandler.fileNameNodes.length, " mutatable nodes. ");
         console.log("In ", ConfigManager.filesToMutate.length, " Files \n");
-        console.log("filename nodes ", this.fileNameNodes);
-        this.supervisor = new Supervisor(this.fileNameNodes);
+        console.log("filename nodes ", this.nodeHandler.fileNameNodes);
+        this.supervisor = new Supervisor(this.nodeHandler.fileNameNodes);
     }
-    // have to do this twice....
-    private createAllFileDescriptors (): Array<IMutatableNode> {
-        const fileDescriptors = new Array<IMutatableNode>();
+
+    // I want to put this somewhere else
+    private createAllFileDescriptors (): Array<IFileDescriptor> {
+        const fileDescriptors = new Array<IFileDescriptor>();
         for (let i = 0; i < ConfigManager.filesToMutate.length; i++) {
             const fo = new FileObject(ConfigManager.filePath, ConfigManager.filesToMutate[i]);
             const fh = new FileHandler(fo);
             const so = new SourceObject(fh.getSourceObject());
             const ci = new CodeInspector(so.origionalSourceObject);
-            this.fileDescriptors.push({
+            fileDescriptors.push({
                     fileName: ConfigManager.filesToMutate[i],
                     fileObject: fo, fileHandler: fh, sourceObject: so, codeInspector: ci
             });
@@ -52,23 +45,6 @@ export class ProfessorX {
         return fileDescriptors;
     }
 
-    private traverseFiles () {
-        for (let i = 0; i < ConfigManager.filesToMutate.length; i++) {
-                this.getAllNodesInFile(this.fileDescriptors[i].codeInspector, i);
-        }
-    }
-
-    private getAllNodesInFile (codeInspector: CodeInspector, i: number): void{
-        MutationFactory.mutatableTokens.forEach((syntaxItem) => {
-            codeInspector.findObjectsOfSyntaxKind(syntaxItem).forEach((token) => {
-                this.fileNameNodes.push({
-                            syntaxType : syntaxItem,
-                            positions : token,
-                            parentFileName: ConfigManager.filesToMutate[i]
-                });
-            });
-        });
-    }
 }
 const x = new ProfessorX();
 x.main();
