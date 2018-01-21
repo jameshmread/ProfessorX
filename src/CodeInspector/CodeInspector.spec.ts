f9e6a6e330ad78de8929255b6f8d229a3583e69a
jameshmread <jamesread117@yahoo.co.uk>
2018-01-21 13:30:44 +0000

    refactored using test utilities

--- src/CodeInspector/CodeInspector.spec.ts
+++ src/CodeInspector/CodeInspector.spec.ts
@@ -1,8 +1,9 @@
-import * as ts from "typescript";
+import { SourceFile, SyntaxKind, Node } from "typescript";
 import { expect } from "chai";
+
 import { CodeInspector } from "./CodeInspector";
-import { SourceFile, SyntaxKind } from "typescript";
 import { SpecificNodeFinder } from "../../testUtilities/SpecificNodeFinder";
+import { SourceObjCreator } from "../../testUtilities/SourceObjCreator";
 
 describe("Testing CodeInspector", () => {
     let code;
@@ -16,45 +17,45 @@ describe("Testing CodeInspector", () => {
             const y: number = 11;
             const z = x+y;
         `;
-        sourceObj = ts.createSourceFile("", code, ts.ScriptTarget.ES5, true);
+        sourceObj = new SourceObjCreator(code).sourceObj;
         ci = new CodeInspector(sourceObj);
         nodeFinder = new SpecificNodeFinder();
     });
 
     it("All plus signs are detected", () => {
-        const actual = ci.findObjectsOfSyntaxKind(ts.SyntaxKind.PlusToken);
+        const actual = ci.findObjectsOfSyntaxKind(SyntaxKind.PlusToken);
         expect(actual.length).to.equal(2);
     });
 
     it("All binary expressions are detected", () => {
-        const actual = ci.findObjectsOfSyntaxKind(ts.SyntaxKind.BinaryExpression);
+        const actual = ci.findObjectsOfSyntaxKind(SyntaxKind.BinaryExpression);
         expect(actual.length).to.equal(3);
     });
 
     it("should return false when + token is between two strings", () => {
         code = "'hello' + 'world';";
-        sourceObj = ts.createSourceFile("", code, ts.ScriptTarget.ES5, true);
+        sourceObj = new SourceObjCreator(code).sourceObj;
         ci = new CodeInspector(sourceObj);
 
-        const allPlusNodes = nodeFinder.findObjectsOfSyntaxKind(ts.SyntaxKind.PlusToken, sourceObj);
-        const plusNode: ts.Node = allPlusNodes[0];
+        const allPlusNodes = nodeFinder.findObjectsOfSyntaxKind(SyntaxKind.PlusToken, sourceObj);
+        const plusNode: Node = allPlusNodes[0];
         const actual = CodeInspector.checkNodeIsMutatable(plusNode);
         expect(actual).to.equal(false);
     });
 
     it("should return true when + token is between two numbers", () => {
         code = "2 + 2;";
-        sourceObj = ts.createSourceFile("", code, ts.ScriptTarget.ES5, true);
+        sourceObj = new SourceObjCreator(code).sourceObj;
         ci = new CodeInspector(sourceObj);
 
-        const allPlusNodes = nodeFinder.findObjectsOfSyntaxKind(ts.SyntaxKind.PlusToken, sourceObj);
-        const plusNode: ts.Node = allPlusNodes[0];
+        const allPlusNodes = nodeFinder.findObjectsOfSyntaxKind(SyntaxKind.PlusToken, sourceObj);
+        const plusNode: Node = allPlusNodes[0];
         const actual = CodeInspector.checkNodeIsMutatable(plusNode);
         expect(actual).to.equal(true);
     });
 
     it("2 plus signs are detected for mutation when 2 valid ones are placed", () => {
-        const actual = ci.findObjectsOfSyntaxKind(ts.SyntaxKind.PlusToken);
+        const actual = ci.findObjectsOfSyntaxKind(SyntaxKind.PlusToken);
         expect(actual.length).to.equal(2);
     });
 });
