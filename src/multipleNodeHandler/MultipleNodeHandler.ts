02ee040fe80280889b3610340ed6a339f058bbd1
jameshmread <jamesread117@yahoo.co.uk>
2018-01-24 21:09:57 +0000

    on failure of source code handler creation, it doesn't mutate the node, instead moves to next mutation

--- src/multipleNodeHandler/MultipleNodeHandler.ts
+++ src/multipleNodeHandler/MultipleNodeHandler.ts
@@ -3,6 +3,7 @@ import { IMutatableNode } from "../../interfaces/IMutatableNode";
 import { OutputStore } from "../../DTOs/OutputStore";
 import { SourceObject } from "../../DTOs/SourceObject";
 import { FileObject } from "../../DTOs/FileObject";
+import { MAttemptFail } from "../../DTOs/MAttemptFail";
 
 import { MutationFactory } from "../mutationFactory/MutationFactory";
 import { OutputStoreManager } from "../output/OutputStoreManager";
@@ -18,41 +19,77 @@ export class MultipleNodeHandler {
       private mochaRunner: MochaTestRunner;
       private sourceCodeHandler: SourceCodeHandler;
       private fileHandler: FileHandler;
+      private currentNode: IMutatableNode;
+      private errorString: string;
+
       constructor () {
             const configManager = new ConfigManager();
             this.outputStoreManager = new OutputStoreManager();
       }
 
-      public async mutateSingleNode (currentNode: IMutatableNode) {
-            const mutationOptions = MutationFactory.getMultipleMutations(currentNode.syntaxType);
-            for (let j = 0; j < mutationOptions.length; j++) {
-                  this.outputStoreManager.setCurrentOutputStore(
-                        new OutputStore(ConfigManager.filePath, currentNode.parentFileName)
-                  ); // file path can be calculated
-                  const fo = new FileObject(ConfigManager.filePath, currentNode.parentFileName);
-                  fo.coreNumber = process.pid;
-                  this.fileHandler = new FileHandler(fo);
-                  const fh = new FileHandler(fo);
-                  this.sourceCodeHandler = new SourceCodeHandler(new SourceObject(fh.getSourceObject()));
+      public setCurrentNode (node: IMutatableNode) {
+            this.currentNode = node;
+      }
 
-                  this.sourceCodeHandler.resetModified(); // resets modified code after a mutation
-                  this.sourceCodeHandler.modifyCode(currentNode, mutationOptions[j]);
-                  // performs the modification at a specific position
-
-                  // writes this change to a NEW src file
-                  const srcFile = this.fileHandler.writeTempSourceModifiedFile(
-                        this.sourceCodeHandler.getModifiedSourceCode());
-                  // creates a new test file with a reference to the NEW source file
-                  const testFile = this.fileHandler.createTempTestModifiedFile();
-
-                  this.outputStoreManager.configureStoreData(testFile, currentNode, this.sourceCodeHandler);
-                  this.mochaRunner = new MochaTestRunner(ConfigManager.runnerConfig);
-                  await this.mochaRunner.runTests(this.outputStoreManager, testFile);
-                  // dont like how im deleting and re creating the test file for every node
-                  this.outputStoreManager.addStoreToList();
-                  Cleaner.deleteTestFile(testFile);
-                  Cleaner.deleteSourceFile(srcFile);
+      public async mutateSingleNode () {
+            const mutationOptions = MutationFactory.getMultipleMutations(this.currentNode.syntaxType);
+            for (let i = 0; i < mutationOptions.length; i++) {
+                  await this.doSingleMutation(mutationOptions[i]);
             }
             Worker.workerResults.push(OutputStoreManager.outputStoreList);
+      }
+
+      private async doSingleMutation (mutationOption: string) {
+            this.outputStoreManager.setCurrentOutputStore(
+                  new OutputStore(ConfigManager.filePath, this.currentNode.parentFileName)
+            ); // file path can be calculated
+            this.setFileInformation();
+
+            if (!this.createSourceCodeHandler()) {
+                  this.outputStoreManager.getCurrentOutputStore().mutationAttemptFailure =
+                  new MAttemptFail(
+                        this.errorString,
+                        this.currentNode.syntaxType.toString() + " => " +
+                        mutationOption,
+                        this.currentNode
+                  );
+                  return;
+            }
+            this.setSourceCodeInformation(mutationOption);
+            // writes this change to a NEW src file
+            const srcFile = this.fileHandler.writeTempSourceModifiedFile(
+                  this.sourceCodeHandler.getModifiedSourceCode());
+            // creates a new test file with a reference to the NEW source file
+            const testFile = this.fileHandler.createTempTestModifiedFile();
+
+            this.outputStoreManager.configureStoreData(testFile, this.currentNode, this.sourceCodeHandler);
+            this.mochaRunner = new MochaTestRunner(ConfigManager.runnerConfig);
+            await this.mochaRunner.runTests(this.outputStoreManager, testFile);
+            // dont like how im deleting and re creating the test file for every node
+            this.outputStoreManager.addStoreToList();
+            Cleaner.deleteTestFile(testFile);
+            Cleaner.deleteSourceFile(srcFile);
+      }
+
+      private setFileInformation () {
+            const fileObject = new FileObject(this.currentNode.parentFileName);
+            fileObject.coreNumber = process.pid;
+            this.fileHandler = new FileHandler(fileObject);
+      }
+
+      private createSourceCodeHandler (): boolean {
+            try {
+                  this.sourceCodeHandler = new SourceCodeHandler(new SourceObject(this.fileHandler.getSourceObject()));
+                  return true;
+            }catch (error) {
+                  this.errorString = "Could not create source code handler: " + error;
+                  console.log(this.errorString);
+                  return false;
+            }
+      }
+
+      private setSourceCodeInformation (mutationOptions: string) {
+            this.sourceCodeHandler.resetModified();
+            this.sourceCodeHandler.modifyCode(this.currentNode, mutationOptions);
       }
 }
