e5f3e4f43fffd106bee210fee0f6e8bdf147552e
jameshmread <jamesread117@yahoo.co.uk>
2018-01-24 21:36:31 +0000

    moved creating file descriptors into node handler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import { ConfigManager } from "./configManager/ConfigManager";
-import { FileHandler } from "./FileHandler/FileHandler";
-import { CodeInspector } from "./CodeInspector/CodeInspector";
 import { Supervisor } from "./supervisor/supervisor";
 import { NodeHandler } from "./nodeHandler/NodeHandler";
-import { SourceObject } from "./../DTOs/SourceObject";
-import { FileObject } from "./../DTOs/FileObject";
-import { IFileDescriptor } from "./../interfaces/IFileDescriptor";
 
 export class ProfessorX {
 
@@ -20,32 +15,15 @@ export class ProfessorX {
 
     public async main () {
         console.log("Creating File Objects");
-        this.nodeHandler = new NodeHandler(this.createAllFileDescriptors());
+        this.nodeHandler = new NodeHandler();
         console.log("Finding Nodes... \n");
         this.nodeHandler.traverseFilesForNodes();
         console.log("Found ", this.nodeHandler.fileNameNodes.length, " mutatable nodes. ");
         console.log("In ", ConfigManager.filesToMutate.length, " Files \n");
-        console.log("filename nodes ", this.nodeHandler.fileNameNodes);
+        console.log("Filename nodes ", this.nodeHandler.fileNameNodes);
         this.supervisor = new Supervisor(this.nodeHandler.fileNameNodes);
         this.supervisor.spawnWorkers();
     }
-
-    // I want to put this somewhere else
-    private createAllFileDescriptors (): Array<IFileDescriptor> {
-        const fileDescriptors = new Array<IFileDescriptor>();
-        for (let i = 0; i < ConfigManager.filesToMutate.length; i++) {
-            const fo = new FileObject(ConfigManager.filesToMutate[i]);
-            const fh = new FileHandler(fo);
-            const so = new SourceObject(fh.getSourceObject());
-            const ci = new CodeInspector(so.origionalSourceObject);
-            fileDescriptors.push({
-                    fileName: ConfigManager.filesToMutate[i],
-                    fileObject: fo, fileHandler: fh, sourceObject: so, codeInspector: ci
-            });
-        }
-        return fileDescriptors;
-    }
-
 }
 const x = new ProfessorX();
 x.main();
