55482fd8c8cfcbcf01f38ce5a3523fa6b728b199
jameshmread <jamesread117@yahoo.co.uk>
2018-02-02 12:11:34 +0000

    added stress configuration

--- profx.conf.ts
+++ profx.conf.ts
@@ -3,8 +3,23 @@ export class Config {
         filePath: "./testProject/src/",
         mutateAllFiles: false,
         filesToMutate: [
+            "HelloWorld.ts",
             "FileTwo.ts",
             "SkipMe.ts"
+        ],
+        filesToSkip: ["SkipMe.ts"],
+        testRunner: "mocha",
+        runnerConfig: {
+            reporter: "dot"
+        }
+    };
+    public static readonly CONFIG_STRESS = {
+        filePath: "./testProject-Stress/src/",
+        mutateAllFiles: false,
+        filesToMutate: [
+            "FileTwo.ts",
+            "FileOne.ts",
+            "LargeFile.ts"
         ],
         filesToSkip: ["SkipMe.ts"],
         testRunner: "mocha",
