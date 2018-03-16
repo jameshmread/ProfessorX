687b02d24ea7171e00a37f91ac474dacadf45530
jameshmread <jamesread117@yahoo.co.uk>
2018-03-16 23:37:43 +0000

    removed unnecessary worker logging

--- src/Worker.ts
+++ src/Worker.ts
@@ -9,7 +9,6 @@ process.on("exit", () => {
 });
 
 import { MultipleNodeMutator } from "./multipleNodeMutator/MultipleNodeMutator";
-import { Logger } from "./logging/Logger";
 
 export class Worker {
       public static workerResults = [];
@@ -24,7 +23,6 @@ export class Worker {
             await this.mutateAllNodes();
             Worker.workerResults = [].concat.apply([], Worker.workerResults);
             process.send(Worker.workerResults);
-            Logger.dumpLogToConsole();
       }
 
       private async mutateAllNodes () {
