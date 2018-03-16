f108b6be81a2de88da26bffad4278d8946a09cf6
jameshmread <jamesread117@yahoo.co.uk>
2018-03-16 23:35:24 +0000

    imutatable nodes now contain src and test file paths

--- interfaces/IMutatableNode.ts
+++ interfaces/IMutatableNode.ts
@@ -2,5 +2,6 @@ export interface IMutatableNode {
      syntaxType: number;
      plainText: string;
      positions: {pos: number, end: number};
-     parentFileName: string;
+     parentFilePath: string;
+     associatedTestFilePath: string;
 }
