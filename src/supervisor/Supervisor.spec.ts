2dcd8126c880b0fd98493c15730a36e306fce016
jameshmread <jamesread117@yahoo.co.uk>
2018-03-24 21:45:29 +0000

    getting individual file results in supervisor has been optimised significantly

--- src/supervisor/Supervisor.spec.ts
+++ src/supervisor/Supervisor.spec.ts
@@ -3,6 +3,7 @@ import { Supervisor } from "./Supervisor";
 import { MutatableNode } from "../../DTOs/MutatableNode";
 import { CreateMutatableNodes } from "../../testUtilities/CreateMutatableNodes";
 import { IMutationResult } from "../../interfaces/IMutationResult";
+import { ConfigManager } from "../configManager/ConfigManager";
 
 describe("Supervisor", () => {
     let sup: Supervisor;
@@ -33,40 +34,40 @@ describe("Supervisor", () => {
 
     beforeEach(() => {
         sup = new Supervisor([fileOneMutatableNode]);
+        ConfigManager.filesToMutate = [];
     });
 
     it("should return one file object with a count of surviving + all mutations", () => {
+        ConfigManager.filesToMutate = ["FileOne.ts"];
         sup.threadResults = [threadResultsFileOne];
-        expect(sup.getIndividualFileResults()).to.eql([{
-            fileName: "FileOne.ts",
-            mutantsSurvived: 1,
-            totalMutationsOnFile: 1
-        }]);
+        expect(sup.getIndividualFileResults()).to.eql({
+            files: ["FileOne.ts"],
+            mutantsSurvivedForEach: [1],
+            totalMutationsForEach: [1]
+        });
     });
 
     it("should return one file object with a count of surviving + all mutations when given 2 results", () => {
+        ConfigManager.filesToMutate = ["FileOne.ts"];
         sup = new Supervisor(
             [fileOneMutatableNode, fileOneMutatableNode]);
         sup.threadResults = [threadResultsFileOne, threadResultsFileOne];
-        expect(sup.getIndividualFileResults()).to.eql([{
-            fileName: "FileOne.ts",
-            mutantsSurvived: 2,
-            totalMutationsOnFile: 2
-        }]);
+        expect(sup.getIndividualFileResults()).to.eql({
+            files: ["FileOne.ts"],
+            mutantsSurvivedForEach: [2],
+            totalMutationsForEach: [2]
+        });
     });
 
     it(`should return two file objects with a count of surviving +
     all mutations when given 2 different results`, () => {
+        ConfigManager.filesToMutate = ["FileOne.ts", "FileTwo.ts"];
         sup = new Supervisor([fileOneMutatableNode, fileTwoMutatableNode]);
         sup.threadResults = [threadResultsFileOne, threadResultsFileTwo];
-        expect(sup.getIndividualFileResults()).to.eql([{
-            fileName: "FileOne.ts",
-            mutantsSurvived: 1,
-            totalMutationsOnFile: 1
-        }, {
-            fileName: "FileTwo.ts",
-            mutantsSurvived: 1,
-            totalMutationsOnFile: 1
-        }]);
+        expect(sup.getIndividualFileResults()).to.eql({
+            files: ["FileOne.ts", "FileTwo.ts"],
+            mutantsSurvivedForEach: [1, 1],
+            totalMutationsForEach: [1, 1]
+        });
     });
 });
