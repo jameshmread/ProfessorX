c675cc6e01dea7889a8a9523291ed132fb7953b3
jameshmread <jamesread117@yahoo.co.uk>
2018-03-28 10:19:13 +0100

    mutation classes now have descriptions

--- enums/MutationClass.ts
+++ enums/MutationClass.ts
@@ -1,5 +1,10 @@
 export enum MutationClass {
-    ForStatement_BoundsChange,
-    NumericLiteral_MultiplyByN1,
-    ReturnEmptyBlock
+    NumericLiteral_ConstantChange = "Modification of Numeric Literal",
+    EmptyBlock = "Remove all content from a block of code",
+    Binary_Substitution = "Replacing one Binary Operator with another",
+    Boolean_Substitution = "Inverting a Boolean",
+    MethodVisibility_Modifier = "Changing the scope / visibility of a method",
+    Unary_Substitution = "Replacing one Unary Operator with another",
+    Parenthesis_Removal = "Removal of Parenthesis in a statement",
+    Return_Null = "Modifying a Return Statement to return null"
 }
