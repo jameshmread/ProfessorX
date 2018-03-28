211ac35ccca131caeb5d3fe9460845f5161a24f8
jameshmread <jamesread117@yahoo.co.uk>
2018-03-28 12:44:28 +0100

    modified interfaces to take new mutation formats (including mutation class)

--- interfaces/IComplexMutation.ts
+++ interfaces/IComplexMutation.ts
@@ -1,5 +1,7 @@
+import { IMutationArrayAndClass } from "./IMutationArrayandClass";
+
 export interface IComplexMutation {
-    getComplexMutation (nodePlainText: string): Array<string>;
+    getComplexMutation (nodePlainText: string): IMutationArrayAndClass;
     isNodeValidForMutation (nodeText: string): boolean;
     removeDuplicateMutations (mutationArray: Array<string>): Array<string>;
 }
