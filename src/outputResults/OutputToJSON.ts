bbd7bc33fe562f624cd49ece0a3b9ae43dda337c
jameshmread <jamesread117@yahoo.co.uk>
2018-03-31 23:24:17 +0100

    output to JSON now produces a unique file for every mutation run

--- src/outputResults/OutputToJSON.ts
+++ src/outputResults/OutputToJSON.ts
@@ -4,32 +4,44 @@ import * as fs from "fs";
 import { EndResult } from "../../DTOs/EndResult";
 import { MathFunctions } from "../maths/MathFunctions";
 import { Logger } from "../logging/Logger";
+import { ConfigManager } from "../configManager/ConfigManager";
 
 export class OutputToJSON {
-      public static writeResults (collatedResults: EndResult) {
-            const header = {
-                runner: collatedResults.runner,
-                config: collatedResults.runnerConf,
-                duration: collatedResults.duration,
-                scoresPerFile: collatedResults.mutationScoresPerFile,
-                overallScores: collatedResults.overallScores
-            };
-            const results = collatedResults.results;
-            const transformStream = JSONStream.stringify();
-            const outputFilePath = "./MutationResults.json";
-            Logger.log("Results File Path", outputFilePath);
-            const outputStream = fs.createWriteStream(outputFilePath);
-            transformStream.pipe(outputStream);
+    public static outputfilePath: string;
 
-            transformStream.write(header);
-            MathFunctions.divideItemsAmongArrays(results,
-                Math.floor(results.length / (results.length / 4))
-            ).forEach((result) => {
-                transformStream.write(result);
-                Logger.info("Length of Division", result.length);
-            });
+    public static writeResults (collatedResults: EndResult) {
+        this.getOutFilePath();
+        const header = {
+            runner: collatedResults.runner,
+            config: collatedResults.runnerConf,
+            duration: collatedResults.duration,
+            scoresPerFile: collatedResults.mutationScoresPerFile,
+            overallScores: collatedResults.overallScores
+        };
+        const results = collatedResults.results;
+        const transformStream = JSONStream.stringify();
+        const outputStream = fs.createWriteStream(OutputToJSON.outputfilePath);
+        Logger.log("Results File Path", OutputToJSON.outputfilePath);
+        transformStream.pipe(outputStream);
 
-            transformStream.end();
-            outputStream.on("finish", () => {Logger.log("Results Written to Disk"); });
-        }
+        transformStream.write(header);
+        MathFunctions.divideItemsAmongArrays(results,
+            Math.floor(results.length / (results.length / 4))
+        ).forEach((result) => {
+            transformStream.write(result);
+            Logger.info("Length of Division", result.length);
+        });
+
+        transformStream.end();
+        outputStream.on("finish", () => {Logger.log("Results Written to Disk"); });
+    }
+
+    private static getOutFilePath () {
+        const rootName = ConfigManager.filePath.split("/")[1];
+        OutputToJSON.outputfilePath = "./" + rootName + "_" +
+        new Date().getDay() + "_" + new Date().getMonth() + "_" + new Date().getFullYear() + "_" +
+        new Date().getUTCHours() + "-" + new Date().getUTCMinutes() + "-" + new Date().getUTCSeconds() +
+        ".json".toString();
+        return OutputToJSON.outputfilePath;
+    }
 }
