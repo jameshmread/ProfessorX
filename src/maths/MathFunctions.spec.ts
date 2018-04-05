85f169d54c0068e3e98da79d111fe65b76561359
jameshmread <jamesread117@yahoo.co.uk>
2018-04-05 14:03:42 +0100

    added more tests for ms converter function in maths functions

--- src/maths/MathFunctions.spec.ts
+++ src/maths/MathFunctions.spec.ts
@@ -29,6 +29,10 @@ describe("Math functions", () => {
             expect(MathFunctions.calculateRunTime(300)).to.eql({ d: 0, h: 0, m: 0, s: 0, ms: 300 });
       });
 
+      it("should set runtime to a date format of 6,14,20,3,1 when given 570003001", () => {
+            expect(MathFunctions.calculateRunTime(570003001)).to.eql({ d: 6, h: 14, m: 20, s: 3, ms: 1 });
+      });
+
       it("should set runtime to 1,1,0,0,0 when given 90000000 (25 hours)", () => {
             expect(MathFunctions.calculateRunTime(90000000)).to.eql({ d: 1, h: 1, m: 0, s: 0, ms: 0 });
       });
